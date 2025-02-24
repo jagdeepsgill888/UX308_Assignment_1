4949e788e904a13edd175629267a51d55818d1e6
Jagdeep Singh <59809683+jagdeepsgill888@users.noreply.github.com>
2025-02-24 00:56:26 +0000

    test most states

--- Order.js
+++ Order.js
@@ -3,22 +3,60 @@ export class Order {
       this.OrderState = {
         WELCOMING: () => {
           let aReturn = [];
-          this.stateCur = this.OrderState.RESERVING;
-          aReturn.push("Welcome to Rich's Acton Rapid Test.");
-          aReturn.push("Would you like to reserve a rapid test kit?");
+          this.stateCur = this.OrderState.SELLING;
+          aReturn.push("Welcome to Friday's Cafe.");
+          aReturn.push("What would you like to order for breakfast today?");
+          aReturn.push("Our all day breakfast menu options are");
+          aReturn.push("Asiago Breakfast Bagel (S/M/L)");
+          aReturn.push("Power Up Breakfast Bagel (S/M/L)");
           return aReturn;
         },
+
+       SELLING: (sInput) => {
+          let aReturn = [];
+          this.stateCur = this.OrderState.UPSELLING;
+          if (sInput.includes('Power up')) {
+            aReturn.push(`Would you like toppings such as bacon for your order?`);
+          } else if (sInput.toLowerCase().includes('asiago')){
+              aReturn.push(`Would you like to add extra cheese to your order?`);
+          } else {
+            aReturn.push(`Sorry we only offer Power Up or Asiago' bagels for breakfast bagels`);
+            this.stateCur = this.OrderState.SELLING;
+          }
+          return aReturn;
+        }, 
+
+        UPSELLING: (sInput) => {
+          let aReturn = [];
+          this.stateCur = this.OrderState.CONFIRM;
+          if (sInput.toLowerCase().includes('yes')) {
+            aReturn.push(`Would you like to add a regular coffee to this order?`);
+          } else {
+            aReturn.push(`No worries, this order will only consist of a bagel`);
+          }
+          return aReturn;
+        }, 
+
+        CONFIRM: (sInput) => {
+          let aReturn = [];
+          this.stateCur = this.OrderState.RESERVING;
+          if (sInput.toLowerCase().includes('yes')) {
+            aReturn.push(`Thank you!! Is that everything for today?`);
+          }
+          return aReturn;
+        }, 
+
         RESERVING: (sInput) => {
           let aReturn = [];
           this.isDone = true;
-          if (sInput.toLowerCase().startsWith('y')) {
-            aReturn.push(`Your rapid test is reserved under the phone number ${this.sFrom}`);
+          if (sInput.toLowerCase().startsWith('yes')) {
+            aReturn.push(`Your breakfast order is reserved under the phone number ${this.sFrom}`);
             let d = new Date();
             d.setMinutes(d.getMinutes() + 120);
-            aReturn.push(`Please pick it up at 123 Tidy St., Acton before ${d.toTimeString()}`);
+            aReturn.push(`Please pick it up at 75 Dalhousie St before ${d.toTimeString()}`);
           } else {
-            aReturn.push("Thanks for trying our reservation system");
-            aReturn.push("Maybe next time")
+            aReturn.push("Thanks for trying Friday's Cafe SMS ordering!");
+            aReturn.push("Have a great day")
           }
           return aReturn;
         }
